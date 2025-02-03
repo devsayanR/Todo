2a969450c6d5fbd73054af4b70a5f0bb107b9757
devsayanR <sayanrakshit.prof@gmail.com>
2025-02-03 08:41:35 +0530

    ui changed of toggle button

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useTodo } from '../context';
+import { FaRegSave, FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import { MdAutoDelete } from "react-icons/md";
 
 const TodoItem = ({ todo }) => {
     const [isTodoEditable, setIsTodoEditable] = useState(false)
@@ -15,18 +18,31 @@ const TodoItem = ({ todo }) => {
         toggleComplete(todo.id)
     }
 
+    useEffect(() => {
+        let timer;
+        if (todo.complete) {
+            timer = setTimeout(() => {
+                deleteTodo(todo.id);
+            }, 5000);
+        }
+        return () => clearTimeout(timer);
+    }, [todo.complete, deleteTodo, todo.id]);
+
     return (
-        <div className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${todo.complete ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"}`} >
-            <input
-                type="checkbox"
-                className="cursor-pointer"
-                checked={todo.complete}
-                onChange={toggleCompleted}
-            />
+        <div className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 duration-300 text-white ${todo.complete ? "bg-[#c6e9a7]" : "bg-yellow-800"}`} >
+            <div className='flex items-center justify-center'>
+                <input
+                    type="checkbox"
+                    className="cursor-pointer border-none outline-none toggle toggle-xs sm:toggle-sm"
+                    checked={todo.complete}
+                    onChange={toggleCompleted}
+                />
+            </div>
 
             <input
                 type="text"
-                className={`border-none outline-none w-full bg-transparent rounded-lg ${isTodoEditable ? "border-black/10 px-2" : "border-transparent"
+                autoFocus={true}
+                className={`border-none outline-none w-full bg-transparent rounded-lg ${isTodoEditable ? "border-black/10 pr-8" : "border-transparent"
                     } ${todo.complete ? "line-through" : ""}`}
                 value={todoMsg}
                 onChange={(e) => setTodoMsg(e.target.value)}
@@ -35,7 +51,7 @@ const TodoItem = ({ todo }) => {
 
             {/* Edit, Save Button */}
             <button
-                className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
+                className="inline-flex w-8 h-8 rounded-lg text-md justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:hidden text-black border-none outline-none"
                 onClick={() => {
                     if (todo.complete) return;
 
@@ -44,14 +60,25 @@ const TodoItem = ({ todo }) => {
                     } else setIsTodoEditable((prev) => !prev);
                 }}
                 disabled={todo.complete}
-            > {isTodoEditable ? "📁" : "✏️"} </button>
+            > {isTodoEditable ? <FaRegSave /> : <FaEdit />} </button>
 
             {/* Delete Todo Button */}
-            <button
-                className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
-                onClick={() => deleteTodo(todo.id)}
-            > ❌ </button>
-        </div>
+            {
+                todo.complete ? (
+                    <button
+                        disabled={true}
+                        title='It will be deleted automatically after 5 minutes'
+                        className="inline-flex w-8 h-8 text-black rounded-lg text-lg border-none outline-none justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
+                        onClick={() => deleteTodo(todo.id)}
+                    > <MdAutoDelete /> </button>
+                ) : (
+                    <button
+                        className="inline-flex w-8 h-8 text-black rounded-lg text-lg border-none outline-none justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
+                        onClick={() => deleteTodo(todo.id)}
+                    > <MdDelete /> </button>
+                )
+            }
+        </div >
     );
 }
 
